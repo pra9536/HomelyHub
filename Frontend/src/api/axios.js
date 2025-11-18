3577f050e798bb3be0e68b684b93394563a2ff29
pra9536 <prateek246729@gmail.com>
2025-11-18 17:36:45 +0530

    Fix formatting in axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,   // Vite uses "VITE_"
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
